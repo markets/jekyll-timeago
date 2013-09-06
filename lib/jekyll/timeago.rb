26c7dbe13d55a6aa987ac11f2a0393c9b69a722c
markets <srmarc.ai@gmail.com>
2013-09-06 22:18:29 +0200

    allows level of detail customization, nice

--- lib/jekyll/timeago.rb
+++ lib/jekyll/timeago.rb
@@ -3,31 +3,44 @@ require "jekyll/timeago/version"
 module Jekyll
   module Timeago
 
-    DAYS_IN = {
+    DAYS_PER = {
       :days => 1,
       :weeks => 7,
       :months => 31,
       :years => 365,
     }
 
-    def timeago(input)
+    # Max level of detail
+    # years > months > weeks > days
+    # 1 year and 7 months and 2 weeks and 6 days
+    MAX_DEPTH_LEVEL = 4
+
+    # Default level of detail
+    # 1 month and 5 days, 3 weeks and 2 days, 2 years and 6 months
+    DEFAULT_DEPTH_LEVEL = 2
+
+    def timeago(input, depth = DEFAULT_DEPTH_LEVEL)
+      unless depth_allowed?(depth)
+        raise "Invalid depth level: #{depth.inspect}"
+      end
+
       unless input.is_a?(Date) || input.is_a?(Time)
         raise "Invalid input type: #{input.inspect}"
       end
 
       days_passed = (Date.today - Date.parse(input.to_s)).to_i
-      time_ago_to_now(days_passed)
+      time_ago_to_now(days_passed, depth)
     end
 
     private
 
-    def time_ago_to_now(days_passed)
+    def time_ago_to_now(days_passed, depth)
       return "today"     if days_passed == 0
       return "yesterday" if days_passed == 1
       return "tomorrow"  if days_passed == -1
 
       future     = days_passed < 0
-      slots      = build_time_ago_slots(days_passed)
+      slots      = build_time_ago_slots(days_passed, depth)
 
       if future
         "in #{slots.join(' and ')}"
@@ -37,11 +50,11 @@ module Jekyll
     end
 
     # Builds time ranges: ['1 month', '5 days']
-    def build_time_ago_slots(days_passed, depth = true)
-      return if days_passed == 0
+    def build_time_ago_slots(days_passed, depth, current_slots = [])
+      return current_slots if depth == 0 || days_passed == 0
 
       time_range = days_to_time_range(days_passed)
-      days       = days_in(time_range)
+      days       = DAYS_PER[time_range]
       num_elems  = days_passed.abs / days
       range_type = if num_elems == 1
         time_range.to_s[0...-1] # singularize
@@ -49,14 +62,9 @@ module Jekyll
         time_range.to_s
       end
 
-      [].tap do |slots|
-        slots << "#{num_elems} #{range_type}" # '1 month', '5 days'
-        if depth
-          pending_days = days_passed - (num_elems*days)
-          depth_slots  = build_time_ago_slots(pending_days, false)
-          slots << depth_slots if depth_slots.any?
-        end
-      end
+      current_slots << "#{num_elems} #{range_type}"
+      pending_days  = days_passed - (num_elems*days)
+      build_time_ago_slots(pending_days, depth - 1, current_slots)
     end
 
     def days_to_time_range(days_passed)
@@ -72,8 +80,8 @@ module Jekyll
       end
     end
 
-    def days_in(time_range)
-      Jekyll::Timeago::DAYS_IN[time_range]
+    def depth_allowed?(depth)
+      (1..MAX_DEPTH_LEVEL).include?(depth)
     end
   end
 end
