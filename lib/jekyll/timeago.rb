2ce923e1692810fcb1c9d24d0a44103ef14d887e
markets <srmarc.ai@gmail.com>
2013-09-06 23:39:34 +0200

    add some comments to code

--- lib/jekyll/timeago.rb
+++ lib/jekyll/timeago.rb
@@ -34,6 +34,7 @@ module Jekyll
 
     private
 
+    # Days passed to time ago sentence
     def time_ago_to_now(days_passed, depth)
       return "today"     if days_passed == 0
       return "yesterday" if days_passed == 1
@@ -52,7 +53,7 @@ module Jekyll
     # Builds time ranges: ['1 month', '5 days']
     # - days_passed: integer in absolute
     # - depth: level of detail
-    # - current_slots: slots built
+    # - current_slots: built time slots
     def build_time_ago_slots(days_passed, depth, current_slots = [])
       return current_slots if depth == 0 || days_passed == 0
 
@@ -70,6 +71,7 @@ module Jekyll
       build_time_ago_slots(pending_days, depth - 1, current_slots)
     end
 
+    # Number of days to minimum period time which can be grouped
     def days_to_time_range(days_passed)
       case days_passed.abs
       when 0...7
@@ -83,6 +85,7 @@ module Jekyll
       end
     end
 
+    # Validate if allowed level of detail
     def depth_allowed?(depth)
       (1..MAX_DEPTH_LEVEL).include?(depth)
     end
